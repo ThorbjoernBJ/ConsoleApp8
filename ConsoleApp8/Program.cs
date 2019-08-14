37d908d59c59b2d46a20106b7af91519c5884a6c
ThorbjoernBJ <53739808+ThorbjoernBJ@users.noreply.github.com>
2019-08-14 11:52:27 +0200

    Update Program.cs

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -7,7 +7,8 @@ namespace ConsoleApp8
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("commit test);
+            Console.WriteLine("commit test");
+            //du kan kun lave kode på GITHUB
         }
     }
 }
